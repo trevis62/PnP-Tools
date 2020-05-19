33f30c53e584cae562a7ad253e5e49dcf7d85a4b
Frode Sivertsen <frode.sivertsen@outlook.com>
2020-05-19 16:46:39 -0700

    cp

--- Solutions/SharePoint.Search.QueryTool/SearchQueryTool/Model/SearchRequest.cs
+++ Solutions/SharePoint.Search.QueryTool/SearchQueryTool/Model/SearchRequest.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using System.Xml.Serialization;
 
